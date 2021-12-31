2be99614e650739a1a38a046944129c2cecbf812
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2021-12-30 20:06:29 -0800

    interactive working single player battleship game

--- src/components/BattleGround.js
+++ src/components/BattleGround.js
@@ -6,50 +6,62 @@ class BattleGround extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			boardData: [[]],
+			player: Player(""),
 		};
 	}
 
 	// Loading a test board (simulates a user placing ships into his board)
 	componentDidMount() {
 		const titoPlayer = Player("tito");
+
 		const yorkTown = Ship("Carrier", 6);
 		const ussr = Ship("Battleship", 3);
 
 		titoPlayer.myBoard.placeShip(yorkTown, 9, 0);
 		titoPlayer.myBoard.placeShip(ussr, 4, 6);
 
-		titoPlayer.myBoard.receiveAttack([9, 0]);
-		titoPlayer.myBoard.receiveAttack([0, 1]);
-		titoPlayer.myBoard.receiveAttack([9, 3]);
+		// titoPlayer.myBoard.receiveAttack([9, 0]);
+		// titoPlayer.myBoard.receiveAttack([0, 1]);
+		// titoPlayer.myBoard.receiveAttack([9, 3]);
 
-		const testBoard = titoPlayer.myBoard.playerBoard;
 		this.setState({
-			boardData: testBoard,
+			player: titoPlayer,
 		});
 	}
 
 	determineSymbol(x, y) {
-		const { boardData } = this.state;
+		const boardData = this.state.player.myBoard.playerBoard;
 		if (boardData[x][y] === null) {
-			return "null";
+			return "--";
 		} else if (boardData[x][y] === -1) {
 			return "miss";
 		} else if (boardData[x][y].isHitHere(x, y)) {
 			return "ship hit";
 		} else {
-			return "ship";
+			return "--";
 		}
 	}
 
 	handleNewAttack(x, y) {
-		alert(`You have clicked on box ${x} and ${y}`);
+		const curPlayer = this.state.player;
+		let result = curPlayer.myBoard.receiveAttack([x, y]);
+		// If it's a successful hit, we will ask the gameboard if all ships are sunk.
+		if (result === 0) {
+			if (this.state.player.myBoard.areShipsSunk()) {
+				alert("All enemy ships have been sunk");
+			}
+		}
+
+		this.setState({
+			player: curPlayer,
+		});
 	}
 
 	generateBoard() {
+		const boardData = this.state.player.myBoard.playerBoard;
 		const board = (
 			<div id="boardRow">
-				{this.state.boardData.map((row, rowKey) => {
+				{boardData.map((row, rowKey) => {
 					return (
 						<div key={rowKey} className="row">
 							{row.map((col, colKey) => {
