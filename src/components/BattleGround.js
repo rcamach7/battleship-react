e997925c640c87d54780ef79069df81f175eb4c4
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-01-05 22:50:01 -0800

    indentation fix

--- src/components/BattleGround.js
+++ src/components/BattleGround.js
@@ -5,149 +5,149 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../styles/BattleGround.css";
 
 class BattleGround extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			player: Player("tito"),
-			computerAi: ComputerAI(),
-			gameStarted: false,
-			ships: [Ship("Yorktown", 6), Ship("Midway", 4), Ship("Tang", 2)],
-			currentShip: 0,
-			normalAxis: true,
-			gameStatus: [
-				"Place Ships on board...",
-				"Attack Enemy Board!",
-				"Victory! WW2 shifts in our favor!",
-				"Defeated by the Japanese!",
-			],
-			gameStatusCode: 0,
-		};
-		this.handleGameStatus = this.handleGameStatus.bind(this);
-		this.handleAutoAttack = this.handleAutoAttack.bind(this);
-		this.handleCurrentShip = this.handleCurrentShip.bind(this);
-		this.toggleAxis = this.toggleAxis.bind(this);
-		this.handleGameStatusCode = this.handleGameStatusCode.bind(this);
-		this.handleComputerChange = this.handleComputerChange.bind(this);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      player: Player("tito"),
+      computerAi: ComputerAI(),
+      gameStarted: false,
+      ships: [Ship("Yorktown", 6), Ship("Midway", 4), Ship("Tang", 2)],
+      currentShip: 0,
+      normalAxis: true,
+      gameStatus: [
+        "Place Ships on board...",
+        "Attack Enemy Board!",
+        "Victory! WW2 shifts in our favor!",
+        "Defeated by the Japanese!",
+      ],
+      gameStatusCode: 0,
+    };
+    this.handleGameStatus = this.handleGameStatus.bind(this);
+    this.handleAutoAttack = this.handleAutoAttack.bind(this);
+    this.handleCurrentShip = this.handleCurrentShip.bind(this);
+    this.toggleAxis = this.toggleAxis.bind(this);
+    this.handleGameStatusCode = this.handleGameStatusCode.bind(this);
+    this.handleComputerChange = this.handleComputerChange.bind(this);
+  }
 
-	handleComputerChange(computerIn) {
-		this.setState({
-			computerAi: computerIn,
-		});
-	}
+  handleComputerChange(computerIn) {
+    this.setState({
+      computerAi: computerIn,
+    });
+  }
 
-	handleAutoAttack(playerIn) {
-		this.setState({
-			player: playerIn,
-		});
-	}
+  handleAutoAttack(playerIn) {
+    this.setState({
+      player: playerIn,
+    });
+  }
 
-	handleGameStatusCode(statusCodeIn) {
-		this.setState({
-			gameStatusCode: statusCodeIn,
-		});
-	}
+  handleGameStatusCode(statusCodeIn) {
+    this.setState({
+      gameStatusCode: statusCodeIn,
+    });
+  }
 
-	handleCurrentShip(currentShipIn) {
-		this.setState({
-			currentShip: currentShipIn,
-		});
-	}
+  handleCurrentShip(currentShipIn) {
+    this.setState({
+      currentShip: currentShipIn,
+    });
+  }
 
-	handleGameStatus() {
-		this.setState({
-			gameStarted: true,
-		});
-	}
+  handleGameStatus() {
+    this.setState({
+      gameStarted: true,
+    });
+  }
 
-	toggleAxis() {
-		if (this.state.normalAxis === true) {
-			this.setState({
-				normalAxis: false,
-			});
-		} else {
-			this.setState({
-				normalAxis: true,
-			});
-		}
-	}
+  toggleAxis() {
+    if (this.state.normalAxis === true) {
+      this.setState({
+        normalAxis: false,
+      });
+    } else {
+      this.setState({
+        normalAxis: true,
+      });
+    }
+  }
 
-	render() {
-		const { player, computerAi, gameStarted, ships, currentShip, normalAxis } =
-			this.state;
-		return (
-			<div className="BattleGround">
-				<p className="gameStatus">
-					{this.state.gameStatus[this.state.gameStatusCode]}
-				</p>
-				{this.state.gameStarted ? null : (
-					<ShipDisplay
-						ships={ships}
-						currentShip={currentShip}
-						normalAxis={normalAxis}
-						toggleAxis={() => this.toggleAxis()}
-					/>
-				)}
-				<div className="battleGround-battleBoard">
-					<MainPlayer
-						currentShip={currentShip}
-						ships={ships}
-						player={player}
-						gameStarted={gameStarted}
-						normalAxis={normalAxis}
-						handleGameStatus={() => this.handleGameStatus()}
-						handleCurrentShip={this.handleCurrentShip}
-						handleGameStatusCode={this.handleGameStatusCode}
-					/>
-					<ComputerAi
-						computerAi={computerAi}
-						gameStarted={gameStarted}
-						player={player}
-						handleAutoAttack={this.handleAutoAttack}
-						handleGameStatusCode={this.handleGameStatusCode}
-						handleComputerChange={this.handleComputerChange}
-					/>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const { player, computerAi, gameStarted, ships, currentShip, normalAxis } =
+      this.state;
+    return (
+      <div className="BattleGround">
+        <p className="gameStatus">
+          {this.state.gameStatus[this.state.gameStatusCode]}
+        </p>
+        {this.state.gameStarted ? null : (
+          <ShipDisplay
+            ships={ships}
+            currentShip={currentShip}
+            normalAxis={normalAxis}
+            toggleAxis={() => this.toggleAxis()}
+          />
+        )}
+        <div className="battleGround-battleBoard">
+          <MainPlayer
+            currentShip={currentShip}
+            ships={ships}
+            player={player}
+            gameStarted={gameStarted}
+            normalAxis={normalAxis}
+            handleGameStatus={() => this.handleGameStatus()}
+            handleCurrentShip={this.handleCurrentShip}
+            handleGameStatusCode={this.handleGameStatusCode}
+          />
+          <ComputerAi
+            computerAi={computerAi}
+            gameStarted={gameStarted}
+            player={player}
+            handleAutoAttack={this.handleAutoAttack}
+            handleGameStatusCode={this.handleGameStatusCode}
+            handleComputerChange={this.handleComputerChange}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 const ShipDisplay = (props) => {
-	return (
-		<div className="shipDisplay">
-			<p className="shipDisplay-status">Current Ship:</p>
-			<p className="shipDisplay-description">
-				Name: {props.ships[props.currentShip].type}
-			</p>
-			<p className="shipDisplay-description">
-				Size: {props.ships[props.currentShip].size}
-			</p>
-			<p className="shipDisplay-description axis">
-				Axis:
-				{props.normalAxis ? (
-					<button onClick={() => props.toggleAxis()}>
-						<FontAwesomeIcon icon="arrows-alt-h" size="1x" />
-					</button>
-				) : (
-					<button onClick={() => props.toggleAxis()}>
-						<FontAwesomeIcon icon="arrows-alt-v" size="1x" />
-					</button>
-				)}
-			</p>
-			<div className="shipDisplay-sizeDisplay">
-				{new Array(props.ships[props.currentShip].size)
-					.fill(0)
-					.map((sizeSquare, i) => {
-						return (
-							<div key={i} className="shipDisplay-sizeDisplay-sizeSquare">
-								<FontAwesomeIcon icon="anchor" size="1x" />
-							</div>
-						);
-					})}
-			</div>
-		</div>
-	);
+  return (
+    <div className="shipDisplay">
+      <p className="shipDisplay-status">Current Ship:</p>
+      <p className="shipDisplay-description">
+        Name: {props.ships[props.currentShip].type}
+      </p>
+      <p className="shipDisplay-description">
+        Size: {props.ships[props.currentShip].size}
+      </p>
+      <p className="shipDisplay-description axis">
+        Axis:
+        {props.normalAxis ? (
+          <button onClick={() => props.toggleAxis()}>
+            <FontAwesomeIcon icon="arrows-alt-h" size="1x" />
+          </button>
+        ) : (
+          <button onClick={() => props.toggleAxis()}>
+            <FontAwesomeIcon icon="arrows-alt-v" size="1x" />
+          </button>
+        )}
+      </p>
+      <div className="shipDisplay-sizeDisplay">
+        {new Array(props.ships[props.currentShip].size)
+          .fill(0)
+          .map((sizeSquare, i) => {
+            return (
+              <div key={i} className="shipDisplay-sizeDisplay-sizeSquare">
+                <FontAwesomeIcon icon="anchor" size="1x" />
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 };
 
 export default BattleGround;
--- src/components/playerGrids.js
+++ src/components/playerGrids.js
@@ -4,180 +4,180 @@ import "../styles/playerGrids.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class MainPlayer extends React.Component {
-	handleNewMove(x, y) {
-		if (!this.props.gameStarted) {
-			this.placeShip(x, y);
-		} else {
-			alert("Time to attack the enemy sir!");
-		}
-	}
+  handleNewMove(x, y) {
+    if (!this.props.gameStarted) {
+      this.placeShip(x, y);
+    } else {
+      alert("Time to attack the enemy sir!");
+    }
+  }
 
-	placeShip(x, y) {
-		const curPlayer = this.props.player;
-		const placedShip = curPlayer.myBoard.placeShip(
-			this.props.ships[this.props.currentShip],
-			x,
-			y,
-			this.props.normalAxis
-		);
+  placeShip(x, y) {
+    const curPlayer = this.props.player;
+    const placedShip = curPlayer.myBoard.placeShip(
+      this.props.ships[this.props.currentShip],
+      x,
+      y,
+      this.props.normalAxis
+    );
 
-		// Evaluate if we have succesfully placed a ship.
-		if (!placedShip) {
-			alert("Ship does not fit here!");
-			return;
-		}
-		this.props.handleCurrentShip(this.props.currentShip + 1);
+    // Evaluate if we have succesfully placed a ship.
+    if (!placedShip) {
+      alert("Ship does not fit here!");
+      return;
+    }
+    this.props.handleCurrentShip(this.props.currentShip + 1);
 
-		// Check if set-up is finished, if so, we start game.
-		if (this.props.currentShip === 2) {
-			this.props.handleGameStatus();
-			this.props.handleGameStatusCode(1);
-		}
-	}
+    // Check if set-up is finished, if so, we start game.
+    if (this.props.currentShip === 2) {
+      this.props.handleGameStatus();
+      this.props.handleGameStatusCode(1);
+    }
+  }
 
-	generateBoard() {
-		const boardData = this.props.player.myBoard.playerBoard;
-		const board = (
-			<div id="boardRow">
-				{boardData.map((row, rowKey) => {
-					return (
-						<div key={rowKey} className="row">
-							{row.map((col, colKey) => {
-								return (
-									<div
-										key={colKey}
-										className="col player-col"
-										onClick={() => this.handleNewMove(rowKey, colKey)}
-									>
-										{this.determineSymbol(rowKey, colKey)}
-									</div>
-								);
-							})}
-						</div>
-					);
-				})}
-			</div>
-		);
-		return board;
-	}
+  generateBoard() {
+    const boardData = this.props.player.myBoard.playerBoard;
+    const board = (
+      <div id="boardRow">
+        {boardData.map((row, rowKey) => {
+          return (
+            <div key={rowKey} className="row">
+              {row.map((col, colKey) => {
+                return (
+                  <div
+                    key={colKey}
+                    className="col player-col"
+                    onClick={() => this.handleNewMove(rowKey, colKey)}
+                  >
+                    {this.determineSymbol(rowKey, colKey)}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
+    return board;
+  }
 
-	determineSymbol(x, y) {
-		const boardData = this.props.player.myBoard.playerBoard;
-		if (boardData[x][y] === null) {
-			return <FontAwesomeIcon icon="water" size="1x" />;
-		} else if (boardData[x][y] === -1) {
-			return "";
-		} else if (boardData[x][y].isHitHere(x, y)) {
-			return <FontAwesomeIcon icon="bullseye" className="hit" size="1x" />;
-		} else {
-			return <FontAwesomeIcon icon="anchor" size="1x" />;
-		}
-	}
+  determineSymbol(x, y) {
+    const boardData = this.props.player.myBoard.playerBoard;
+    if (boardData[x][y] === null) {
+      return <FontAwesomeIcon icon="water" size="1x" />;
+    } else if (boardData[x][y] === -1) {
+      return "";
+    } else if (boardData[x][y].isHitHere(x, y)) {
+      return <FontAwesomeIcon icon="bullseye" className="hit" size="1x" />;
+    } else {
+      return <FontAwesomeIcon icon="anchor" size="1x" />;
+    }
+  }
 
-	render() {
-		return (
-			<div className="mainPlayer">
-				<p className="playerNames">United States Navy</p>
-				{this.generateBoard()}
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="mainPlayer">
+        <p className="playerNames">United States Navy</p>
+        {this.generateBoard()}
+      </div>
+    );
+  }
 }
 
 class ComputerAi extends React.Component {
-	componentDidMount() {
-		// Set up japanese fleet
-		const computerAISetup = this.props.computerAi;
+  componentDidMount() {
+    // Set up japanese fleet
+    const computerAISetup = this.props.computerAi;
 
-		const ships = [Ship("Akagi", 6), Ship("Mikuma", 4), Ship("Yamato", 2)];
-		computerAISetup.myBoard.placeShip(ships[0], 0, 0, true);
-		computerAISetup.myBoard.placeShip(ships[1], 5, 3, true);
-		computerAISetup.myBoard.placeShip(ships[2], 7, 4, true);
+    const ships = [Ship("Akagi", 6), Ship("Mikuma", 4), Ship("Yamato", 2)];
+    computerAISetup.myBoard.placeShip(ships[0], 0, 0, true);
+    computerAISetup.myBoard.placeShip(ships[1], 5, 3, true);
+    computerAISetup.myBoard.placeShip(ships[2], 7, 4, true);
 
-		this.props.handleComputerChange(computerAISetup);
-	}
+    this.props.handleComputerChange(computerAISetup);
+  }
 
-	receiveAttack(x, y) {
-		// Will not allow an attack unless game has been started or if attempting to striking a location already struck.
-		if (!this.props.gameStarted) {
-			return;
-		} else if (this.props.computerAi.myBoard.isRepeatedAttack([x, y])) {
-			return;
-		}
+  receiveAttack(x, y) {
+    // Will not allow an attack unless game has been started or if attempting to striking a location already struck.
+    if (!this.props.gameStarted) {
+      return;
+    } else if (this.props.computerAi.myBoard.isRepeatedAttack([x, y])) {
+      return;
+    }
 
-		// Sends attack AND records if it hit a ship. Then, If it's a successful hit, we will ask the game board if all ships are sunk.
-		const curComputer = this.props.computerAi;
-		let attemptAttack = curComputer.myBoard.receiveAttack([x, y]);
-		if (attemptAttack === 0) {
-			// Check if the successful attack has won the game.
-			if (curComputer.myBoard.areShipsSunk()) {
-				this.props.handleGameStatusCode(2);
-			}
-		}
-		this.props.handleComputerChange(curComputer);
-		this.returnAttack();
-	}
+    // Sends attack AND records if it hit a ship. Then, If it's a successful hit, we will ask the game board if all ships are sunk.
+    const curComputer = this.props.computerAi;
+    let attemptAttack = curComputer.myBoard.receiveAttack([x, y]);
+    if (attemptAttack === 0) {
+      // Check if the successful attack has won the game.
+      if (curComputer.myBoard.areShipsSunk()) {
+        this.props.handleGameStatusCode(2);
+      }
+    }
+    this.props.handleComputerChange(curComputer);
+    this.returnAttack();
+  }
 
-	returnAttack() {
-		// Now that we have received an attack, it's our turn to send out an attack.
-		const curPlayer = this.props.player;
-		const generateAttack = this.props.computerAi.generateAttack();
-		curPlayer.myBoard.receiveAttack(generateAttack);
+  returnAttack() {
+    // Now that we have received an attack, it's our turn to send out an attack.
+    const curPlayer = this.props.player;
+    const generateAttack = this.props.computerAi.generateAttack();
+    curPlayer.myBoard.receiveAttack(generateAttack);
 
-		this.props.handleAutoAttack(curPlayer);
-		// Handle computer win
-		if (curPlayer.myBoard.areShipsSunk()) {
-			this.props.handleGameStatusCode(3);
-		}
-	}
+    this.props.handleAutoAttack(curPlayer);
+    // Handle computer win
+    if (curPlayer.myBoard.areShipsSunk()) {
+      this.props.handleGameStatusCode(3);
+    }
+  }
 
-	generateBoard() {
-		const boardData = this.props.computerAi.myBoard.playerBoard;
-		const board = (
-			<div id="boardRow">
-				{boardData.map((row, rowKey) => {
-					return (
-						<div key={rowKey} className="row">
-							{row.map((col, colKey) => {
-								return (
-									<div
-										key={colKey}
-										className="col computer-col"
-										onClick={() => this.receiveAttack(rowKey, colKey)}
-									>
-										{this.determineSymbol(rowKey, colKey)}
-									</div>
-								);
-							})}
-						</div>
-					);
-				})}
-			</div>
-		);
-		return board;
-	}
+  generateBoard() {
+    const boardData = this.props.computerAi.myBoard.playerBoard;
+    const board = (
+      <div id="boardRow">
+        {boardData.map((row, rowKey) => {
+          return (
+            <div key={rowKey} className="row">
+              {row.map((col, colKey) => {
+                return (
+                  <div
+                    key={colKey}
+                    className="col computer-col"
+                    onClick={() => this.receiveAttack(rowKey, colKey)}
+                  >
+                    {this.determineSymbol(rowKey, colKey)}
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
+    return board;
+  }
 
-	determineSymbol(x, y) {
-		const boardData = this.props.computerAi.myBoard.playerBoard;
-		if (boardData[x][y] === null) {
-			return <FontAwesomeIcon icon="water" size="1x" />;
-		} else if (boardData[x][y] === -1) {
-			return "";
-		} else if (boardData[x][y].isHitHere(x, y)) {
-			return <FontAwesomeIcon icon="bullseye" className="hit" size="1x" />;
-		} else {
-			return <FontAwesomeIcon icon="water" size="1x" />;
-		}
-	}
+  determineSymbol(x, y) {
+    const boardData = this.props.computerAi.myBoard.playerBoard;
+    if (boardData[x][y] === null) {
+      return <FontAwesomeIcon icon="water" size="1x" />;
+    } else if (boardData[x][y] === -1) {
+      return "";
+    } else if (boardData[x][y].isHitHere(x, y)) {
+      return <FontAwesomeIcon icon="bullseye" className="hit" size="1x" />;
+    } else {
+      return <FontAwesomeIcon icon="water" size="1x" />;
+    }
+  }
 
-	render() {
-		return (
-			<div>
-				<p className="playerNames">Japanese Empire Navy</p>
-				<div className="computerAiGrid">{this.generateBoard()}</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <p className="playerNames">Japanese Empire Navy</p>
+        <div className="computerAiGrid">{this.generateBoard()}</div>
+      </div>
+    );
+  }
 }
 
 export { ComputerAi, MainPlayer };
