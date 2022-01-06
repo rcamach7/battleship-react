8a0b39c6540c6509f7f5c4ced909969d2a5f1b03
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-01-05 22:48:55 -0800

    indentation fix

--- src/App.js
+++ src/App.js
@@ -4,36 +4,36 @@ import BattleGround from "./components/BattleGround";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import {
-	faWater,
-	faBullseye,
-	faAnchor,
-	faRedo,
-	faArrowsAltH,
-	faArrowsAltV,
+  faWater,
+  faBullseye,
+  faAnchor,
+  faRedo,
+  faArrowsAltH,
+  faArrowsAltV,
 } from "@fortawesome/free-solid-svg-icons";
 
 class App extends React.Component {
-	render() {
-		return (
-			<div className="App">
-				<header className="title-container">
-					<h1>Battle Of Midway</h1>
-				</header>
+  render() {
+    return (
+      <div className="App">
+        <header className="title-container">
+          <h1>Battle Of Midway</h1>
+        </header>
 
-				<BattleGround />
-			</div>
-		);
-	}
+        <BattleGround />
+      </div>
+    );
+  }
 }
 
 library.add(
-	fab,
-	faWater,
-	faBullseye,
-	faAnchor,
-	faRedo,
-	faArrowsAltH,
-	faArrowsAltV
+  fab,
+  faWater,
+  faBullseye,
+  faAnchor,
+  faRedo,
+  faArrowsAltH,
+  faArrowsAltV
 );
 
 export default App;
